ada0c1d613443d437d2dfaa81a4a0ce3a12c90de
Sergey Kibish <sergey.kibish@gmail.com>
2015-11-20 20:45:33 +0200

    connect charger with logic, initialize vending machine in at interface start

--- interface/server.js
+++ interface/server.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var request = require('request');
 
-var apiServerHost = process.env.LOGIC_URL
+var apiServerHost = process.env.LOGIC_URL;
+var chargerUrl = process.env.CHARGER_URL;
+var fillerUrl = process.env.FILLER_URL
 
 var app = express();
+
+var options = {
+  method: 'POST',
+  url: apiServerHost + '/init',
+  body: {filler_url: fillerUrl, charger_url: chargerUrl},
+  json: true
+};
+
+// vending machine initialization
+request(options);
 
 app.use(express.static('public'));
 
