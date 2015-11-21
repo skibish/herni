8f9831cd32c031590417a5b4547f0329dfd00db9
Sergey Kibish <sergey.kibish@gmail.com>
2015-11-21 23:24:10 +0200

    Describe actions and action types

---
+++ interface/app/actions/actions.js
@@ -0,0 +1,118 @@
+import * as types from '../constants/ActionTypes.js';
+
+export function requestProducts() {
+  return {
+    type: types.REQUEST_PRODUCTS,
+  };
+}
+
+export function receiveProducts(json) {
+  return {
+    type: types.RECEIVE_PRODUCTS,
+    products: json.products
+  };
+}
+
+export function fetchProducts() {
+  return dispatch => {
+    dispatch(requestProducts());
+    return $.ajax({
+      url: '/api/product_list',
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        dispatch(receiveProducts(data));
+        return true;
+      }
+    });
+  }
+}
+
+export function selectItem(id) {
+  return {
+    type: types.SELECT_ITEM,
+    id
+  };
+}
+
+export function enableBuyButton() {
+  return {
+    type: types.ENABLE_BUY_BUTTON
+  };
+}
+
+export function disableBuyButton() {
+  return {
+    type: types.DISABLE_BUY_BUTTON
+  };
+}
+
+export function sendCash(amount) {
+  return {
+    type: types.SEND_CASH,
+    amount
+  };
+}
+
+export function receiveCash(amount) {
+  type: types.RECEIVE_CASH,
+  amount
+}
+
+export function requestCash(amount) {
+  return dispatch => {
+    dispatch(sendCash(amount));
+    return $.ajax({
+      url: '/api/balance_refill',
+      method: "POST",
+      data: JSON.stringify({credit: parseFloat(amount)}),
+      dataType: "json",
+      contentType: "application/json; charset=utf-8",
+      success: function (data) {
+        dispatch(receiveCash(Math.round(data.credit * 100) / 100));
+      }
+    });
+  }
+}
+
+export function receiveMessage(mesage) {
+  return {
+    type: types.RECEIVE_MESSAGE,
+    message
+  };
+}
+
+export function buySelectedItem(id) {
+  type: BUY_SELECTED_ITEM,
+  id
+}
+
+export function requestBuy(id, type = 'cash', paymentDetails = {}) {
+  return dispatch => {
+    dispatch(buySelectedItem(id));
+    return $.ajax({
+      url: '/api/purchase',
+      method: 'POST',
+      data: JSON.stringify({slot: id, payment: type, payment_details: paymentDetails),
+      dataType: "json",
+      contentType: "application/json; charset=utf-8",
+      success: function (data) {
+        let credit = Math.round(data.credit * 100) / 100;
+        if (type == 'cash') {
+          dispatch(receiveCash(Math.round(data.credit * 100) / 100));
+        }
+        dispatch(receiveMessage(data.result));
+      }
+    });
+  }
+}
+
+export function addCreditCard(ccnum) {
+  type: types.ADD_CREDIT_CARD,
+  ccnum
+}
+
+export function addCardPin(pin) {
+  type: types.ADD_CARD_PIN,
+  pin
+}
