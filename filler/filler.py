35db5909c6511e94a564bc3ff04169e04ae5ce1c
andrey.toumilovich <pervozvannij@inbox.lv>
2016-01-04 21:28:19 +0200

    Changing Filler class to Singleton

--- filler/filler.py
+++ filler/filler.py
@@ -8,39 +8,62 @@ app = Flask(__name__)
 
 logic_port = "4567"
 products = [{"name": "snickers",
-             "price": 0.80},
+             "price": 80},
             {"name": "cola",
-             "price": 0.100},
+             "price": 100},
             {"name": "water",
-             "price": 0.75},
+             "price": 75},
             {"name": "orange juice",
-             "price": 0.100},
+             "price": 100},
             {"name": "unknown juice",
-             "price": 0.65},
+             "price": 65},
             {"name": "milk",
-             "price": 0.85},
+             "price": 85},
             {"name": "Semki",
-             "price": 0.77},
+             "price": 77},
             {"name": "nuts",
-             "price": 0.120},
+             "price": 120},
             {"name": "waffels",
-             "price": 0.110},
+             "price": 110},
             {"name": "banana",
-             "price": 0.25},
+             "price": 25},
             {"name": "potato",
-             "price": 0.50},
+             "price": 50},
             {"name": "poison",
-             "price": 0.45}]
+             "price": 45}]
 
+class Singleton:
 
-def refill_slot(url, data):
-    print "Filling after some delay..."
-    time.sleep(10)
-    print "Filling logic slot %d" % data['product']['slot']
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-    r = requests.post(url, data=json.dumps(data), headers=headers)
-    if r.status_code != 200:
-        raise Exception("Logic returned %d" % r.status_code)
+    def __init__(self, decorated):
+        self._decorated = decorated
+
+    def instance(self):
+        try:
+            return self._instance
+        except AttributeError:
+            self._instance = self._decorated()
+            return self._instance
+
+    def __call__(self):
+        raise TypeError('Singletons must be accessed through `instance()`.')
+
+    def __instancecheck__(self, inst):
+        return isinstance(inst, self._decorated)
+
+@Singleton
+class Filler:
+
+    def __init__(self):
+        pass
+
+    def refill_slot(self, url, data):
+        print "Filling after some delay..."
+        time.sleep(10)
+        print "Filling logic slot %d" % data['product']['slot']
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        if r.status_code != 200:
+            raise Exception("Logic returned %d" % r.status_code)
 
 
 class RestServer:
@@ -66,13 +89,14 @@ class RestServer:
             d['slot'] = i
             d['count'] = capacity
             prod_list.append(d)
+        print prod_list
         result = {'products': prod_list}
         return json.dumps(result)
 
     # UI
     @staticmethod
     @app.route('/empty', methods=['POST'])
-    def product_list():
+    def fill():
         data = request.get_json(silent=True)
         url = "http://" + request.remote_addr + ":" + logic_port + "/slot_refill"
         index = data['product_id']
@@ -81,7 +105,7 @@ class RestServer:
                          'price': products[index]['price'],
                          'slot': data['slot'],
                          'count': data['capacity']}}
-        thread.start_new_thread(refill_slot, (url, d))
+        thread.start_new_thread(Filler.instance().refill_slot, (url, d))
         return ""
 
 if __name__ == '__main__':
